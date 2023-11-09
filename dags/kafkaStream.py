1a19e394b18e376ea460001a569dc0dcc47cf990
Chề Trung Nguyên <nguyen7obu@gmail.com>
2023-11-10 02:24:21 +0700

    done airflow task

--- dags/kafkaStream.py
+++ dags/kafkaStream.py
@@ -1,15 +1,15 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
+import json
+import logging
 import requests
 from kafka import KafkaProducer
-import json
 import time
-import logging
 
 default_args = {
     'owner': 'che',
-    'start_date': datetime(2023, 11, 8, 8, 00)
+    'start_date': datetime(2023, 11, 8)
 }
 
 def fetch_data(date):
@@ -25,44 +25,47 @@ def fetch_data(date):
 def realtime_task():
     today = datetime.today().strftime('%Y-%m-%d')
 
-    producer = KafkaProducer(bootstrap_servers = 'localhost:9092',
+    producer = KafkaProducer(bootstrap_servers = ['broker:29092'],
                             max_block_ms = 5000)
+    logging.info('Connection complete')
 
-    while True:
-        try:
-            res = fetch_data(today)
-            producer.send('raw_data', json.dumps(res).encode('utf-8'))
-            logging.info('Send to Kafka')
-        except Exception as e:
-            logging.error(f'An error occured: {e}')
-            continue
-        time.sleep(60)
+    try:
+        res = fetch_data(today)
+        producer.send('raw_realtime', json.dumps(res).encode('utf-8'))
+        logging.info('Send to Kafka')
+    except Exception as e:
+        logging.error(f'An error occured: {e}')
 
 def daily_task():
     date = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 
-    producer = KafkaProducer(bootstrap_servers = 'localhost:9092',
+    producer = KafkaProducer(bootstrap_servers = ['broker:29092'],
                             max_block_ms = 5000)
     
     try:
         res = fetch_data(date)
-        producer.send('raw_data', json.dumps(res).encode('utf-8'))
+        producer.send('raw_daily', json.dumps(res).encode('utf-8'))
         logging.info('Send to Kafka')
     except Exception as e:
         logging.error(f'An error occured: {e}')
 
 
-with DAG('user_automation',
+with DAG('daily_dag',
          default_args= default_args,
-         schedule='@daily',
+         schedule_interval='0 1 * * 2-6',
          catchup= False) as dag:
-
-    realtime_streaming_task = PythonOperator(
-        task_id = 'realtime_streaming_task',
-        python_callable= realtime_task
-    )
 
     daily_streaming_task = PythonOperator(
         task_id = 'daily_streaming_task',
         python_callable= daily_task
+    )
+
+with DAG('realtime_dag',
+         default_args= default_args,
+         schedule_interval='* 1-7 * * 1-5',
+         catchup= False) as dag:
+    
+    realtime_streaming_task = PythonOperator(
+        task_id = 'realtime_streaming_task',
+        python_callable= realtime_task
     )
